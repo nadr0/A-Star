18a69c1c261c2b435fa2efa1d71547570cf6a705
Kevin Nadro <sodapopcuffgames@gmail.com>
2014-06-18 12:39:50 -0500

    Adding node class
    -Added the node
    -Created the data array 80x40 to draw all the nodes

---
+++ node.js
@@ -0,0 +1,31 @@
+var node = Class.extend({
+            init: function(PositionX, PositionY){
+                // The x/y position is the upper left of the square
+                this.posX = PositionX;
+                this.posY = PositionY;
+
+                // The center of the node
+                this.centerX = this.posX + (nodesize/2);
+                this.centerY = this.posY + (nodesize/2);
+
+                // Width/Height of the square to be drawn
+                this.width = nodesize;
+                this.height = nodesize;
+
+                // Color
+                this.color = 'AliceBlue';
+
+                // The adjacent nodes to this node
+                this.northnode = null;
+                this.southnode = null;
+                this.eastnode = null;
+                this.westnode = null;
+            },
+            draw: function(){
+                context.beginPath();
+                context.fillStyle = this.color;
+                context.fillRect(this.posX, this.posY, this.width, this.height);
+                context.closePath();
+            }
+
+        });
